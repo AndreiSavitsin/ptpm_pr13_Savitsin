6bdc41d413ddd7bfa5a0e3aed2112709fb28389f
AndreiSavitsin <andreeeeewcs@gmail.com>
2026-03-18 14:38:10 +0500

    Add files via upload

---
+++ ptpm_pr13_Savitsin/ClassStudents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ptpm_pr9_Savitsin
+{
+    internal class ClassStudents
+    {
+        static public List<string> ListStudents = new List<string>(); //Лист студентов
+    }
+}
